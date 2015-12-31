5bd38613dc6a4d936f56e9fdd211e1a60f58a5ea
Roberto Filho <roberto.w.filho@gmail.com>
2015-12-31 15:53:10 -0200

    Adicionei uma configuração.

---
+++ org.filho.product.plugin/src/org/filho/product/plugin/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,20 @@
+package org.filho.product.plugin;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "org.filho.product.plugin.perspective";
+
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+
+}
